b49c204911811ad1bb366fd5a5b97df522b00a03
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-30 19:40:59 -0300

    classes corpo e chão + gravidade

--- Classes.py
+++ Classes.py
@@ -1,12 +1,50 @@
 import pygame
+
 from pygame.locals import *
 from sys import exit
 import random as rdm
 
-class Chão:
-    def __init__(self,pos=tuple,L=int,A=int,surf="superficie/tela"):
-        self.x = pos[0]
-        self.y = pos[1]
-        self.L = L
-        self.A = A
-        self.surf = surf
+elementos = pygame.sprite.Group()
+allgnds = pygame.sprite.Group()
+
+class Chão(pygame.sprite.Sprite):
+    def __init__(self, tam = tuple, pos=tuple) :
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = pos
+        self.tam = tam
+        self.rect = (pos[0],pos[1],tam[0],tam[1])
+        self.add(allgnds)
+        self.add(elementos)
+
+    def update(self):
+
+        return 
+
+        
+
+class Corpo(pygame.sprite.Sprite):
+   
+   def __init__(self, tam = tuple, pos=tuple) :
+        pygame.sprite.Sprite.__init__(self)
+        self.x=pos[0]
+        self.y=pos[1]
+        self.pos = (self.x,self.y)
+        self.tam = tam
+        self.rect = Rect(pos , tam)
+        self.add(elementos)
+
+        
+
+   def update(self):
+        self.Fall = True
+
+        for gnd in allgnds:
+            if  pygame.sprite.collide_rect(self,gnd):
+                self.Fall= False
+        if self.Fall == True:    
+            self.y += 2
+            self.pos = (self.x,self.y)
+            self.rect = Rect(self.pos , self.tam)
+       
+        return
+           
--- Principal.py
+++ Principal.py
@@ -6,24 +6,43 @@ from pygame.locals import *
 from sys import exit
 import random as rdm
 
+
 pygame.init()
 tela = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0],FULLSCREEN)
-
 play = True
 
+
+
+
+ch1 = Chão((tela.get_width(),50),(0,tela.get_height() - 50 ))
+ch2 = Chão((tela.get_width()/2,50),(100,tela.get_height() -500 ))
+corpo1 = Corpo((50,50),(tela.get_width()/2,0))
+corpo1.add(elementos)
+
+
+
+
 while play:
-    pygame.display.flip()
+
     tela.fill((255,255,255))
 
     #Analisa eventos
     for event in pygame.event.get():
         if event.type == QUIT:
+           
+
             play = False
             pygame.quit()
             exit()
+    
         
     
-
+    pygame.draw.rect(tela,(0,0,0),ch1.rect)
+    pygame.draw.rect(tela,(0,0,0),ch2.rect)
+    pygame.draw.rect(tela,(255,0,0),corpo1.rect)
+    
+    elementos.update()
+    pygame.display.flip()
 
 
 
