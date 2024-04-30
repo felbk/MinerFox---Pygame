9b5a229183ae6d1b4a0fca7e5780c97f4f31f4d6
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-30 12:51:24 -0300

    testes

--- Grafico.py
+++ Grafico.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 from sys import exit
 
 def inicia_tela(L=int,A=int):
-    tela = pygame.display
-    tela.init()
-    tela.set_mode((L,A))
+    pygame.display.init()
+    tela = pygame.display.set_mode((L,A))
+    
     return tela
--- Principal.py
+++ Principal.py
@@ -3,16 +3,21 @@ from Classes import *
 import pygame
 from pygame.locals import *
 from sys import exit
+import random as rdm
 
 pygame.init()
 tela=inicia_tela(500,500)
-
+cor_tela= (0,0,150)
 play = True
 
 while play:
-    tela.flip()
+    tela.fill(cor_tela)
+    pygame.display.flip()
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             play != play
             exit()
+        if event.type == KEYDOWN:
+
     
