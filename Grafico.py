95448f8ba3707abe117ef2861b188663190a7913
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-30 12:30:07 -0300

    sem loop infinito (testes)

--- Grafico.py
+++ Grafico.py
@@ -3,3 +3,8 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+def inicia_tela(L=int,A=int):
+    tela = pygame.display
+    tela.init()
+    tela.set_mode((L,A))
+    return tela
--- Principal.py
+++ Principal.py
@@ -4,7 +4,15 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+pygame.init()
+tela=inicia_tela(500,500)
+
 play = True
 
 while play:
+    tela.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play != play
+            exit()
     
