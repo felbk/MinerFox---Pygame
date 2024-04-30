bf61219a29458cae99da8b22a01d27066a5a1ca6
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-30 12:16:51 -0300

    Criação dos arquivos

---
+++ Grafico.py
@@ -0,0 +1,5 @@
+from Classes import*
+import pygame
+from pygame.locals import *
+from sys import exit
+
---
+++ Principal.py
@@ -0,0 +1,10 @@
+from Grafico import *
+from Classes import *
+import pygame
+from pygame.locals import *
+from sys import exit
+
+play = True
+
+while play:
+    
