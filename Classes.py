c967b4381a081f2b5c5091dfb69b76f7a83486ad
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-30 20:42:50 -0300

    classe player e movimentação inicial

--- Classes.py
+++ Classes.py
@@ -1,8 +1,8 @@
 import pygame
-
 from pygame.locals import *
 from sys import exit
 import random as rdm
+
 
 elementos = pygame.sprite.Group()
 allgnds = pygame.sprite.Group()
@@ -31,20 +31,60 @@ class Corpo(pygame.sprite.Sprite):
         self.pos = (self.x,self.y)
         self.tam = tam
         self.rect = Rect(pos , tam)
+        self.vx = 0
+        self.vy = 0
         self.add(elementos)
 
         
 
    def update(self):
+        
         self.Fall = True
+        from Principal import Be
+        from Principal import Bd
 
+        if Bd:
+            self.vx = 0.5
+        elif Be:
+            self.vx = -0.5
+        else:
+            self.vx= 0 
+            
+        # Confere se está no chão ou caindo
         for gnd in allgnds:
             if  pygame.sprite.collide_rect(self,gnd):
                 self.Fall= False
-        if self.Fall == True:    
-            self.y += 2
-            self.pos = (self.x,self.y)
-            self.rect = Rect(self.pos , self.tam)
-       
+                    
+        if self.Fall == True:
+            self.vy = 2
+        else:
+            self.vy = 0
+
+        #atualiza posição
+        self.y += self.vy
+        self.x += self.vx
+        self.pos = (self.x,self.y)
+        self.rect = Rect(self.pos , self.tam)
+        
         return
+
+class Player(Corpo):
+    def __init__(self,tam, pos):
+        Corpo.__init__(self,tam,pos)
+        
+
+    def update(self):
+        # Controle para andar
+        from Principal import Be
+        from Principal import Bd
+
+        if Bd:
+            self.vx = 0.5
+        elif Be:
+            self.vx = -0.5
+        else:
+            self.vx= 0 
+        Corpo.update(self)
+        return
+
            
--- Principal.py
+++ Principal.py
@@ -10,17 +10,21 @@ import random as rdm
 pygame.init()
 tela = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0],FULLSCREEN)
 play = True
-
+clock = pygame.time.Clock()
+clock.tick(30)
 
 
 
 ch1 = Chão((tela.get_width(),50),(0,tela.get_height() - 50 ))
 ch2 = Chão((tela.get_width()/2,50),(100,tela.get_height() -500 ))
-corpo1 = Corpo((50,50),(tela.get_width()/2,0))
+corpo1 = Player((50,50),(tela.get_width()/2,0))
 corpo1.add(elementos)
 
 
 
+
+Bd = False
+Be = False
 
 while play:
 
@@ -34,6 +38,23 @@ while play:
             play = False
             pygame.quit()
             exit()
+        if event.type == KEYDOWN:
+                if event.key == K_d: 
+                    Bd = True
+                else:
+                     Bd = False
+                if event.key == K_a: 
+                    Be = True
+                else:
+                     Be = False
+        if event.type == KEYUP:
+                if event.key == K_d: 
+                    Bd = False
+                if event.key == K_a: 
+                    Be = False
+        
+                
+       
     
         
     
